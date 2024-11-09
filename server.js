cd56d9112d5c948829561a4c0a0677dfdb970438
Grace Aliko <gracealiko09@gmail.com>
2024-11-09 22:49:55 +0000

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const User = require('./models/User')
 const authRoutes = require('./routes/authRoutes');
